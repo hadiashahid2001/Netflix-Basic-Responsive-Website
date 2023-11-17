a02fd4681d0c24c24ee092da4cc0cdb1dc0a18e4
Hadia Shahid <hadiashahidwah@gmail.com>
2023-11-17 18:13:16 +0500

    New Web

---
+++ jsFile/NetFlix_main.js
@@ -0,0 +1,8 @@
+// for Video
+function toggleVideo(){
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    trailer.classList.toggle('active')
+    video.currentTime = 0;
+    video.pause();
+}    
